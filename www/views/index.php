057951861406eeaa4a45b0a34460b6851ff16acd
Ilya <asus72@yandex.ru>
2015-02-18 22:59:09 +0300

    Изменен index.php

--- www/views/index.php
+++ www/views/index.php
@@ -5,11 +5,12 @@
     <title>Новостной портал</title>
 </head>
 <body>
-<div> <p></div>
+<div></div>
     <?php while($news = mysql_fetch_assoc($res)):?>
       <?php echo $news['header'];?>
       <?php echo $news['content'];?>
         <a href="<?php echo $news['path'];?>">Читать новость</a>
     <?php endwhile; ?>
+<div id="right_menu"><a href="../views/add_news.php">Добавить новость</a></div>
 </body>
 </html>
